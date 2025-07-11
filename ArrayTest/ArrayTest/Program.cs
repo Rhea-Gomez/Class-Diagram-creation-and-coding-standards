fcef04bdccdc4dfa7c331e32854842295ebaa396
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 16:02:54 +0530

    ArrayTest.cs

---
+++ ArrayTest/ArrayTest/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ArrayTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colours = { "red", "blue", "green", "yellow", "violet", "white" };
+
+            Console.WriteLine("Using For loop: ");
+            for(int i = 0; i < colours.Length; i++)
+            {
+                Console.WriteLine(colours[i]);
+            }
+
+            Console.WriteLine("Using For Each: ");
+            foreach(string colour in colours)
+            {
+                Console.WriteLine(colour);
+            }
+        }
+    }
+}
